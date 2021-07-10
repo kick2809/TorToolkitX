f8a8ec63c3143aac11adefadee587b94bc6873fd
kick2809 <83411241+kick2809@users.noreply.github.com>
2021-07-10 22:33:58 +0530

    Update status.py

--- tortoolkit/core/status/status.py
+++ tortoolkit/core/status/status.py
@@ -63,10 +63,17 @@ class QBTask(Status):
         return self._omess.sender_id
 
     async def create_message(self):
-        msg = "<b>╭──────── ⌊Downloading:</b> <code>{}</code>\n".format(
+        msg += f"\n<b>╭──────── ⌊ 📥 Downloading ⌉ </b>"
+        msg += f"\n<b>│</b>"
+        msg += "<b>├Progress:</b> {} - {}%\n".format(
+            self.progress_bar(self._torrent.progress),
+            round(self._torrent.progress*100,2)
+            )
+        msg += f"\n<b>│</b>"
+        msg = "<b>📚:</b> <code>{}</code>\n".format(
             self._torrent.name[:26]
             )
-        msg += "<b>├Down:</b> {} <b>Up:</b> {}\n".format(
+        msg += "<b>├Speed 🚀:</b> {} <b>Up:</b> {}\n".format(
             human_readable_bytes(self._torrent.dlspeed,postfix="/s"),
             human_readable_bytes(self._torrent.upspeed,postfix="/s")
             )
@@ -74,17 +81,17 @@ class QBTask(Status):
             self.progress_bar(self._torrent.progress),
             round(self._torrent.progress*100,2)
             )
-        msg += "<b>├Downloaded:</b> {} of {}\n".format(
+        msg += "<b>├Total Size 🗂:</b> {} of {}\n".format(
             human_readable_bytes(self._torrent.downloaded),
             human_readable_bytes(self._torrent.total_size)
             )
-        msg += "<b>├ETA:</b> <b>{}</b>\n".format(
+        msg += "<b>├ETA ⏳ : </b> <b>{}</b>\n".format(
             human_readable_timedelta(self._torrent.eta)
             )
-        msg += "<b>├S:</b>{} <b>L:</b>{}\n".format(
+        msg += "<b>├🌱:</b>{} <b>├🍐:</b>{}\n".format(
             self._torrent.num_seeds,self._torrent.num_leechs
             )
-        msg += "<b>╰─── ⌊ ⚡️Using engine:</b> <code>qBittorrent</code>"
+        msg += "<b>╰─── ⌊ ⚡️Using engine:</b> ⌉ <code>qBittorrent</code>"
 
         return msg
 
