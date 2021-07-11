685a92293ea093a01549f09fd86d263356474a72
kick2809 <83411241+kick2809@users.noreply.github.com>
2021-07-11 22:42:35 +0530

    Update status.py

--- tortoolkit/core/status/status.py
+++ tortoolkit/core/status/status.py
@@ -67,7 +67,7 @@ class QBTask(Status):
         msg += "<b>├📚:</b> <code>{}</code>\n".format(
             self._torrent.name[:30]
             )
-        msg += "<b>├</b>
+        msg += "<b>├</b>"
            )
         msg += "<b>├Speed 🚀:</b> {} <b>Up:</b> {}\n".format(
             human_readable_bytes(self._torrent.dlspeed,postfix="/s"),
