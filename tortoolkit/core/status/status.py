07817737b3bd10cb16840ac253c3b5444f913227
kick2809 <83411241+kick2809@users.noreply.github.com>
2021-07-11 22:14:28 +0530

    Update status.py

--- tortoolkit/core/status/status.py
+++ tortoolkit/core/status/status.py
@@ -65,7 +65,7 @@ class QBTask(Status):
     async def create_message(self):
         msg = f"\n<b>╭────── ⌊ 📥 Downloading ⌉ </b>"
             )
-        msg = "<b>├📚:</b> <code>{}</code>\n".format(
+        msg += "<b>├📚:</b> <code>{}</code>\n".format(
             self._torrent.name[:65]
             )
         msg += "<b>├</b>
